631bedb07e9b83d18e30f459a6c9a78cf1368231
Rotem Kellner <rkellner@augury.com>
2017-01-10 20:35:47 +0200

    why sit days

--- imports/ui/ar.js
+++ imports/ui/ar.js
@@ -29,20 +29,20 @@ Template.AR.helpers({
     },
    incompleteCount() {
        return manipulateCount(Ars.find());
-   },
-
-   /* //csv export
-    'click #buttonDownload': function(Ars) {
-        var nameFile = 'fileDownloaded.csv';
-        Meteor.call('download', function(err, fileContent) {
-            if(fileContent){
-                var blob = new Blob([fileContent], {type: "text/plain;charset=utf-8"});
-                saveAs(blob, nameFile);
-            }
-        })}*/
+   }
 
 });
 
+Template.AR.events({
+  'click #buttonDownload': function(Ars) {
+    var nameFile = 'fileDownloaded.csv';
+    Meteor.call('download', function(err, fileContent) {
+      if(fileContent){
+        var blob = new Blob([fileContent], {type: "text/plain;charset=utf-8"});
+        saveAs(blob, nameFile);
+      }})
+  }
+});
 
 var manipulateCount = function (ars) {
     var count = 0;
--- server/main.js
+++ server/main.js
@@ -122,13 +122,13 @@ Meteor.methods({
         }
     },
     //CSV exprt
-    /*
+
     download: function() {
         var collection = Ars.find().fetch();
         var heading = true; // Optional, defaults to true
         var delimiter = ";" // Optional, defaults to ",";
         return exportcsv.exportToCSV(collection, heading, delimiter);
-   }*/
+    }
 });
 
 Accounts.onCreateUser(function(options, user) {

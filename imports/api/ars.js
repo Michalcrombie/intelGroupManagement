fa53e0c6dc2fd515492aa966aab0c9d1bf685e51
Hana Matatov <hanama888@gmail.com>
2017-01-11 17:43:01 +0200

    editing values & obligations on api -> ars.js

--- imports/api/ars.js
+++ imports/api/ars.js
@@ -3,66 +3,64 @@
 export const Ars = new Mongo.Collection("ars");
 
 Ars.attachSchema(new SimpleSchema({
-  description: {
-    type: String,
-    label: "AR description",
-    max: 200,
-    defaultValue: "description" ,
-  },
-  srartDate: {
-      type: Date,
-      label: "Start Date",
-      defaultValue: moment().format() ,
-  },
-  dueDate: {
-      type: Date,
-      label: "Due Date",
-      defaultValue: moment().format() ,
-  },
-  catagory: {
-      type: String,
-      label: "catagory",
-      allowedValues: ["Services","KPIs","HW"],
-      defaultValue:"Services"
-  },
-  subCatagory: {
-      type: String,
-      label: "Sub Catagory",
-      allowedValues: ["TLC","Thermal","DP"],
-      defaultValue:"TLC"
-  },
-  priorty: {
-    type: Number,
-    label: "Priorty",
-    min: 0,
-    defaultValue:0 ,
-  },
-  owner: {
-    type: String,
-    label: "Owner",
-    defaultValue:"none" ,
-  },
-  seconderyOwner: {
-      type: String,
-      label: "Secondery Owner",
-      defaultValue:"none" ,
-  },
-  status: {
-      type: String,
-      label: "Status",
-      allowedValues: ["Open","In process","Done"],
-      defaultValue:"Open",
-  },
-  statusDetails: {
-      type: String,
-      label: "Status Details",
-      defaultValue:"none" ,
-  },
-  comments: {
-    type: String,
-    label: "Comments",
-    optional: true,
-    max: 1000,
-    defaultValue:"none" ,
-  }
+    description: {
+        type: String,
+        label: "AR Description",
+        max: 500
+    },
+    catagory: {
+        type: String,
+        label: "Category",
+        allowedValues: ["Services","KPIs","HW", "Marketing", "All", "TEMP"],
+        optional: true
+    },
+    subCatagory: {
+        type: String,
+        label: "Sub Category",
+        allowedValues: ["Thermal", "DP", "Regulatory", "TpT", "Methodology", "Tools", "MAC", "PHY", "Power", "Coex", "Uploads"],
+        optional: true
+    },
+    srartDate: {
+        type: Date,
+        label: "Start Date",
+        optional: true
+    },
+    dueDate: {
+        type: Date,
+        label: "Due Date",
+        optional: true
+    },
+    priorty: {
+        type: Number,
+        label: "Priorty",
+        min: 1,
+        max: 5
+    },
+    owner: {
+        type: String,
+        label: "Owner",
+        optional: true
+    },
+    seconderyOwner: {
+        type: String,
+        label: "Secondary Owner",
+        optional: true
+    },
+    status: {
+        type: String,
+        label: "Status",
+        allowedValues: ["Open","WIP", "Done", "Deferred"],
+        optional: true
+    },
+    statusDetails: {
+        type: String,
+        label: "Status Details",
+        optional: true
+    },
+    comments: {
+        type: String,
+        label: "Comments",
+        optional: true,
+        max: 1000
+    }
 }));

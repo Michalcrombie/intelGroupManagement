bc0e0282003dfaf73ae26538f197ca6a726dc509
Hana Matatov <hanama888@gmail.com>
2017-03-05 14:13:28 +0200

    Algorithm works!

--- imports/ui/ar-add.js
+++ imports/ui/ar-add.js
@@ -7,9 +7,86 @@ Template.ARAdd.onCreated(function bodyOnCreated() {
     this.state = new ReactiveDict();
 });
 
+
+Template.ARAdd.events({
+    "change input": function (e,t) {
+        Session.set(e.target.name, e.target.value);
+
+        set_owner("owner");
+        set_owner("seconderyOwner");
+
+
+    },
+    "change select[name!='owner'][name!='seconderyOwner']": function(e, t) {
+        Session.set(e.target.name, e.target.value);
+        set_owner("owner");
+        set_owner("seconderyOwner");
+    }
+
+});
+
+var collection_users = Meteor.users;
+
+// AR template record
+function AR(action_num, description, category, sub_category, owner, secondary_owner, start_date, due_date, priorty) {
+    this.action_num = action_num;
+    this.description = description;
+    this.category = category;
+    this.sub_category = sub_category;
+    this.owner = owner;
+    this.secondary_owner = secondary_owner;
+    this.start_date = start_date;
+    this.due_date = due_date;
+    this.priorty = priorty;
+}
+
+// Initialize an AR for the algorithm
+function init_curr_ar() {
+    var catagory = Session.get("catagory");
+    var sub_catagory = Session.get("subCatagory");
+    var start_date = new Date(Session.get("srartDate"));
+    var due_date = new Date(Session.get("dueDate"));
+
+    if (catagory != null && sub_catagory != null && start_date != null && due_date != null) {
+        return new AR(null, null, catagory, sub_catagory, null, null, start_date, due_date, null);
+    } else {
+        return null;
+    }
+
+}
+
+function set_owner(field_name){
+    var curr_ar = init_curr_ar();
+    if (curr_ar != null) {
+
+        console.log(curr_ar);
+
+        Meteor.call('get_owners', curr_ar, function(error, result){
+            var options = result.slice();
+            console.log($('[name="'+field_name+'"]').attr("id"));
+            var s = $("<select id=\""+$('[name="'+field_name+'"]').attr("id")+"\" name=\""+field_name+"\" data-schema-key=\""+field_name+"\" autocomplete=\"off\" class=\"form-control\" />");
+            options.forEach(function(e){
+                $("<option />", {value: e.name, text: e.name}).appendTo(s);
+                // console.log(e);
+            });
+
+            //
+
+            var div = $('[name="'+field_name+'"]').parent('div');
+            $('[name="'+field_name+'"]').remove();
+            div.append(s);
+
+
+        });
+
+    }
+}
+
 Template.ARAdd.helpers({
     ars() {
         return Ars;
     },
+    userschema() {
+        return userSchema;
+    }
 });
-
--- server/main.js
+++ server/main.js
@@ -7,14 +7,14 @@ Meteor.startup(() => {
     // code to run on server at startup
     Tasks = new Mongo.Collection("tasks");
 
-  smtp = {
-    username: 'intelgroupmanagment@gmail.com',
-    password: '12345678intel',
-    server:   'smtp.gmail.com',
-    port: 465
-  }
+    smtp = {
+        username: 'intelgroupmanagment@gmail.com',
+        password: '12345678intel',
+        server:   'smtp.gmail.com',
+        port: 465
+    }
 
-  process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
+    process.env.MAIL_URL = 'smtp://' + encodeURIComponent(smtp.username) + ':' + encodeURIComponent(smtp.password) + '@' + encodeURIComponent(smtp.server) + ':' + smtp.port;
 });
 
 Meteor.methods({
@@ -31,24 +31,213 @@ Meteor.methods({
             });
         };  
     },
-   insertIntelusers: function(doc) {
+    get_owners: function(curr_ar) {
+
+        function AR(action_num, description, category, sub_category, owner, secondary_owner, start_date, due_date, priorty) {
+            this.action_num = action_num;
+            this.description = description;
+            this.category = category;
+            this.sub_category = sub_category;
+            this.owner = owner;
+            this.secondary_owner = secondary_owner;
+            this.start_date = start_date;
+            this.due_date = due_date;
+            this.priorty = priorty;
+        }
+
+        console.log("running publish");
+        var qualified = [];
+        Meteor.users.find().fetch().forEach(function(elem) {
+            console.log("iterating");
+            if (typeof elem.qualifications !== 'undefined') {
+                if (elem.qualifications ==  curr_ar.category  || elem.sub_qualifications == curr_ar.sub_category) {
+                    //console.log(elem.first_name );
+                    qualified.push(elem.username);
+                };
+            }
+
+        });
+        console.log(qualified);
+        if (!qualified.isEmpty) {
+            // console.log(qualified);
+            var tasks = []
+            qualified.forEach(function (username) {
+                //console.log(qualified);
+                var relevant_tasks = [];
+                var temp_tasks = Ars.find({owner: username}).fetch();
+                // console.log("tasks for " + username);
+                //console.log(temp_tasks);
+                //console.log(temp_tasks);
+                for (j = 0, len = temp_tasks.length; j < len; j++) {
+                    var i = temp_tasks[j];
+
+                    if ((i.srartDate > curr_ar.due_date) || (i.dueDate < curr_ar.start_date)) {
+                        continue;
+                    }
+
+                    else if ((i.srartDate <= curr_ar.start_date) && (i.dueDate <= curr_ar.due_date)) {
+                        i.srartDate = curr_ar.start_date;
+                        relevant_tasks.push(i);
+                    }
+
+                    else if ((i.srartDate >= curr_ar.start_date) && (i.dueDate >= curr_ar.due_date)) {
+                        i.dueDate = curr_ar.due_date;
+                        relevant_tasks.push(i);
+                    }
+
+                    else if ((i.srartDate <= curr_ar.start_date) && (i.dueDate >= curr_ar.due_date)) {
+                        i.srartDate = curr_ar.start_date;
+                        i.dueDate = curr_ar.due_date;
+                        relevant_tasks.push(i);
+                    }
+
+                    else if ((i.srartDate >= curr_ar.start_date) && (i.dueDate <= curr_ar.due_date)) {
+                        relevant_tasks.push(i);
+                    }
+                }
+                //console.log("relevant tasks for " + username);
+                // console.log(relevant_tasks);
+                tasks.push(relevant_tasks);
+
+            });
+            console.log(tasks);
+            function calculate_length(An_AR) {
+                var oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+                var length = 0;
+                length = Math.round(Math.abs((An_AR.srartDate.getTime() - An_AR.dueDate.getTime()) / (oneDay)));
+                return length;
+            }
+
+
+            var name_weight = [];
+            //function for calculate all tasks weight of one employee
+            function sum_of_weight(tasks) {
+                for (i = 0; i < tasks.length; i++) {
+                    var sum = 0;
+                    for (j = 0; j < tasks[i].length; j++) {
+                        var weight = (calculate_length(tasks[i][j])) * (tasks[i][j].priorty);
+                        sum += weight;
+                        if (j == (tasks[i].length) - 1) {
+                            name_weight.push({name: tasks[i][j].owner, weight: sum});
+                        }
+                    }
+                }
+            }
+
+            (sum_of_weight(tasks));
+
+            var name_numOfArs = [];
+
+            function sum_of_ars(tasks) {
+                for (i = 0; i < tasks.length; i++) {
+                    var count = 0;
+                    for (j = 0; j < tasks[i].length; j++) {
+                        count++;
+                        var x = tasks[i][j].owner;
+                    }
+                    name_numOfArs.push({name: x, num: count});
+                }
+                return name_numOfArs;
+            }
+
+            sum_of_ars(tasks);
+
+            var final_array = []
+
+            //sort the employees according to they sum
+            function best_match(name_numOfArs, name_weight) {
+                for (i = 0; i < name_numOfArs.length; i++) {
+                    for (t = 0; t < name_weight.length; t++) {
+                        if (name_numOfArs[i].name == name_weight[t].name) {
+                            var x = (name_numOfArs[i].num) * 10;
+                            var final_value = x + name_weight[t].weight;
+                            final_array.push({name: name_numOfArs[i].name, weight: final_value});
+                        }
+                    }
+                }
+                return final_array;
+            }
+
+            best_match(name_numOfArs, name_weight);
+            //console.log("final_array")
+            //console.log(final_array)
+
+            function sort(final_array) {
+                function dynamicSort(property) {
+                    var sortOrder = 1;
+                    if (property[0] === "-") {
+                        sortOrder = -1;
+                        property = property.substr(1);
+                    }
+                    return function (a, b) {
+                        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+                        return result * sortOrder;
+                    }
+                }
+
+                final_array.sort(dynamicSort("weight"));
+            }
+
+            var final = [];
+            var len = final_array.length;
+            for (j = (len - 1); j >= 0; j--) {
+                final.push(final_array[j].name);
+            }
+
+
+
+            var returned_array = []
+            final_array.forEach(function (elem) {
+                returned_array.push(elem.name);
+            });
+            /*
+               console.log(final);
+   
+               console.log(Meteor.users.find({"username": {"$in": returned_array}}).fetch());
+   */
+            console.log(final_array);
+
+            function search(nameKey, myArray){
+                for (var i=0; i < myArray.length; i++) {
+                    if (myArray[i].name === nameKey) {
+                        return myArray[i];
+                    }
+                }
+            }
+
+            qualified.forEach(function(user){
+                //var result = $.grep(final_array, function(e) {return e.name == user;})
+                if (!search(user, final_array)){
+                    final_array.push({name: user, weight: 0});
+                }
+
+            });
+
+            var unique_final_array = _.uniq(final_array, function(e){return e.name;});
+            sort(unique_final_array);
+            console.log(unique_final_array);
+            return unique_final_array;
+        }
+        return [];
+    },
+    insertIntelusers: function(doc) {
         Meteor.users.update({_id: Meteor.userId()}, {$set: doc});
-   },
-   sendmail(doc) {
-       var usersnew = Meteor.users.find();
-       usersnew.forEach(function(user){
-           if (user.first_name==doc.owner)
-           {
-               var mail=user.email_adress;
-               Email.send({       
-                   to: mail,
-                   from: "intelgroupmanagment@gmail.com",
-                   subject: "Message From Intel WIFI core system architecture ",
-                   text: "Hello, Your AR was edited by:"+ Meteor.user().first_name
-            })
-           }
-       });
-   },
+    },
+    sendmail(doc) {
+        var usersnew = Meteor.users.find();
+        usersnew.forEach(function(user){
+            if (user.first_name==doc.owner)
+            {
+                var mail=user.email_adress;
+                Email.send({       
+                    to: mail,
+                    from: "intelgroupmanagment@gmail.com",
+                    subject: "Message From Intel WIFI core system architecture ",
+                    text: "Hello, Your AR was edited by:"+ Meteor.user().first_name
+                })
+            }
+        });
+    },
 });
 
 Meteor.methods({
@@ -105,43 +294,50 @@ Meteor.methods({
     }
 });
 
+
 Accounts.onCreateUser(function(options, user) {
-  var newFields = {
-    employee_id: "",
-    first_name: "",
-    last_name: "",
-    office_address: "",
-    email_adress: "",
-    qualifications: "",
-    sub_qualifications: "",
-    Permission:""
-  };
-  _.extend(user, newFields);
-  return user;
+    var newFields = {
+        employee_id: "",
+        first_name: "",
+        last_name: "",
+        office_address: "",
+        email_adress: "",
+        qualifications: "",
+        sub_qualifications: "",
+        Permission:""
+    };
+    _.extend(user, newFields);
+    return user;
 });
 
 Meteor.publish(null, function() {
 
-  if (this.userId != null) {
-    return Meteor.users.find({
-      _id: this.userId
-    }, {
-        fields: {
-            'employee_id':1,
-            'first_name': 1,
-            'last_name':1,
-            'office_address':1,
-            'email_adress':1,
-            'qualifications':1,
-            'sub_qualifications':1,
-            'Permission':1,
-      },
+    if (this.userId != null) {
+        return Meteor.users.find({
+            _id: this.userId
+        }, {
+            fields: {
+                'employee_id':1,
+                'first_name': 1,
+                'last_name':1,
+                'office_address':1,
+                'email_adress':1,
+                'qualifications':1,
+                'sub_qualifications':1,
+                'Permission':1,
+            },
   
-    });
-  } else {
-    return this.ready();
-  }
+        });
+    } else {
+        return this.ready();
+    }
 });
+
+
+/*
+Meteor.publish("qualification.public", function(curr_ar) {
+
+});*/
 
 Ars.find().observe ({
     changed: function (newDocument, oldDocument){
@@ -157,4 +353,4 @@ Ars.find().observe ({
             if (oldDocument.owner != newDocument.owner){
                 Changes.insert({ ArID:oldDocument._id,ownerChanged:true , dueDateChanged:false })
             }}
-}})
+    }})
